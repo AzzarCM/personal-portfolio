35dbc75c0e651ed665313c23f06cf2d24937a884
Cristian Mundo <00086917@uca.edu.sv>
2022-01-10 18:42:59 -0600

    some change idk

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,11 +1,5 @@
-import { useEffect } from 'react'
 import Home from "./pages/Home";
-import Aos from "aos";
-import 'aos/dist/aos.css'
 export default function App() {
-  useEffect(() => {
-    Aos.init( {duration: 2000 });
-  }, [])
   return (
     <Home/>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,32 @@
 import React, { useEffect } from "react";
 import Header from "../components/Header";
 import "./custom.css";
-import Aos from 'aos'
-import 'aos/dist/aos.css'
+import AOS from "aos";
+import "aos/dist/aos.css";
 import ProjectsGalery from "../components/ProjectsGalery";
 import AboutMe from "../components/AboutMe";
 import Welcome from "../components/Welcome";
 
 const Home = () => {
+  useEffect(() => {
+    AOS.init({duration: 3000});
+  }, []);
   return (
     <>
       <div className="bg-gradient-to-r from-custom-black to-custom-black-gray h-screen">
         <Header />
-        <Welcome/>
+        <Welcome />
       </div>
-      <div className="bg-custom-black-200 p-5">
-        <p data-aos="flip-up" className="text-5xl md:text-6xl lg:text-9xl text-right pr-10 font-bold text-custom-black-gray mt-12">{`<projects>`}</p>
+      <div style={{overflowX: 'hidden'}} className="bg-custom-black-200 p-5">
+        <p
+          data-aos="flip-up"
+          className="text-5xl md:text-6xl lg:text-9xl text-right pr-10 font-bold text-custom-black-gray mt-12"
+        >{`<projects>`}</p>
         <ProjectsGalery/>
-        <p data-aos="slide-right" className="text-5xl md:text-6xl lg:text-9xl text-center pr-10 font-bold text-custom-black-gray">{`<about me>`}</p>
+        <p
+          data-aos="slide-right"
+          className="text-5xl md:text-6xl lg:text-9xl text-center pr-10 font-bold text-custom-black-gray"
+        >{`<about me>`}</p>
         <AboutMe/>
       </div>
     </>
